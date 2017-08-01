ebe8b42baca7072566a3384c7f354b7a3c12e712
amrinaX <amrina9501@gmail.com>
2017-08-01 17:26:43 +0800

    lab2
    delete

--- lab2-final/div_inst.vhd
+++
@@ -1,6 +0,0 @@
-div_inst : div PORT MAP (
-		denom	 => denom_sig,
-		numer	 => numer_sig,
-		quotient	 => quotient_sig,
-		remain	 => remain_sig
-	);
--- lab2-final/mul_inst.vhd
+++
@@ -1,5 +0,0 @@
-mul_inst : mul PORT MAP (
-		dataa	 => dataa_sig,
-		datab	 => datab_sig,
-		result	 => result_sig
-	);
